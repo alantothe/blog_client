d6dfcd3203790ac5efe39b20fba10aa126d2a7c8
alantothe <rubenamalpartida@gmail.com>
2023-05-18 22:37:50 -0500

    login and register working

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,15 +1,65 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
+import { useAuth } from '../hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, isLoggedIn, checkLoginStatus } = useAuth();
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    checkLoginStatus();
+    if (isLoggedIn) {
+      navigate('/dashboard');
+    }
+  }, [isLoggedIn, navigate, checkLoginStatus]);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const result = await login(email, password);
+      if (result.success) {
+        navigate('/dashboard')
+      } else {
+        console.log("Error")
+      }
+    }
+    catch (error) {
+        console.log("Error", error)
+      }
+
+  };
+
+  const handleRedirect = () => {
+    navigate('/register')
+  }
 
   return (
-   <div>
-    <h1>LoginPage</h1>
-   </div>
+    <section>
+      <header>
+        <h1>Login Page</h1>
+      </header>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" value={email} onChange={handleEmailChange} />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+
+        <button type="submit">Login</button>
+      </form>
+      <button onClick={handleRedirect}>Register?</button>
+    </section>
   );
 };
-
 
 export default LoginPage;
--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,15 +1,94 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
-const RegisterPage = () => {
+function RegistrationPage() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRegister = async () => {
+    try {
+      const user = {
+        username: username,
+        email: email,
+        password: password
+      };
+
+      const response = await axios.post(`${process.env.REACT_APP_URL_ENDPOINT}/api/user/register`, user);
+
+      if (response.data.success) {
+        navigate('/login');
+      } else {
+        console.log('Error');
+      }
+    } catch (error) {
+      console.log('Error');
+    }
+  };
+
+
+  const handleRedirect = () => {
+    navigate('/login');
+  };
 
 
   return (
-   <div>
-    <h1>RegisterPage</h1>
-   </div>
+    <div className="container">
+      <h1>Registration Page</h1>
+
+      <div className="formGroup">
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div className="formGroup">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div className="formGroup">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+
+      <button type="submit" onClick={handleRegister} className="button">
+        Register
+      </button>
+       <button onClick={handleRedirect} className="button">
+        Login?
+      </button>
+    </div>
   );
-};
+}
+
+export default RegistrationPage;
 
 
-export default RegisterPage;
